cf0464a28b9d07a7429c1a4ebec504c2a1cd9dd8
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-01 15:59:56 +0900

    Node.jsを使って、HTTPサーバーを作成

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict';
+const http = require('http');
+// サーバーを作成
+const server = http.createServer((req, res) => {
+  // HTTPレスポンスヘッダを書き込む
+  res.writeHead(200, {
+    'Content-Type': 'text/plain; charset=utf-8'
+  });
+  // HTTPレスポンスの内容（本文）を書きだす
+  res.write(req.headers['user-agent']);
+  // レスポンスの書き出しを終了
+  res.end();
+});
+const port = 8000;
+
+// HTTPサーバーを起動
+server.listen(port, () => {
+  console.log(`ポート ${port} 番でサーバーを起動しました`);
+});
