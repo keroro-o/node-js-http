36bdd243b6b83318bec0362b4b319efe2e6ca7f9
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-01 16:09:47 +0900

    HTTPレスポンスの本文をHTMLに変更

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   // HTTPレスポンスヘッダを書き込む
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
   // HTTPレスポンスの内容（本文）を書きだす
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
   // レスポンスの書き出しを終了
   res.end();
 });
