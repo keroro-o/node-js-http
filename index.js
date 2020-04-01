b59349b4e25333ea2696ed9d627959d97cb48969
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-02 01:52:13 +0900

    POSTメソッドとGETメソッドを実装

--- index.js
+++ index.js
@@ -3,21 +3,38 @@ const http = require('http');
 // サーバーを作成
 const server = http.createServer((req, res) => {
   // 情報のログとエラーログをコンソールに出力
-  console.info(`[${new Date()}] Requested by ${req.connection.remoteAddress}`);
+  const now = new Date();
+  console.info(`[${now}] Requested by ${req.connection.remoteAddress}`);
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+
+  switch (req.method) {
+    case 'GET':
+      res.write('GET ' + req.url);
+      break;
+    case 'POST':
+      res.write('POST ' + req.url);
+      let rawData = '';
+      req.on('data', (chunk) => {
+        rawData = rawData + chunk;
+      }).on('end', () => {
+        console.info('[' + now + '] Data posted: ' + rawData);
+      });
+      break;
+    default:
+      break;
+  }
   res.end();
 }).on('error', (e) => {
-  console.error(`[${new Date()}] Server Error`, e);
+  console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
-  console.error(`[${new Date()}] Client Error`, e);
+  console.error('[' + new Date() + '] Client Error', e);
 });
 
 const port = 8000;
 
 // HTTPサーバーを起動
 server.listen(port, () => {
-  console.info(`[${new Date()}] ポート ${port} 番でサーバーを起動しました`);
+  console.info(`[${new Date()}]ポート ${port} 番でサーバーを起動しました`);
 });
