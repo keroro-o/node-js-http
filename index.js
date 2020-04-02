03f83c6363351dcc02a956466145a561a3ab3fa5
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-02 23:38:05 +0900

    HTMLフォームを作成し、HTTPサーバーにGETでアクセスした際に、このフォームを表示するように実装

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const server = http.createServer((req, res) => {
   const now = new Date();
   console.info(`[${now}] Requested by ${req.connection.remoteAddress}`);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      res.write(`GET ${req.url}`);
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
       break;
     case 'POST':
       res.write(`POST ${req.url}`);
@@ -20,6 +22,7 @@ const server = http.createServer((req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         console.info(`[${now}] Data posted: ${rawData}`);
+        res.end();
       });
       break;
     case 'DELETE':
@@ -28,7 +31,7 @@ const server = http.createServer((req, res) => {
     default:
       break;
   }
-  res.end();
+
 }).on('error', (e) => {
   console.error(`[${new Date()}] Server Error`, e);
 }).on('clientError', (e) => {
