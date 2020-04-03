680b58d59a68d35fd839cf67fb42a73a4cb61f51
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-04 01:45:22 +0900

    '/logout'にアクセスした際の処理を実装

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const basic = auth.basic(
 const server = http.createServer(basic, (req, res) => {
   // 情報のログとエラーログをコンソールに出力
   console.info(`Requested by ${req.connection.remoteAddress}`);
+
+  // パスが'/logout'の時に、ステータスコード401-Unauthorized を返す
+  if (req.url === '/logout') {
+    res.writeHead(401, {
+      'Content-Type': 'text/plain; charset=utf-8'
+    });
+    res.end('ログアウトしました');
+    return;
+  }
+
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
